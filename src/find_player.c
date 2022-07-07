c3abeb4408f5e9caa2aaaccb772941f20071c0f2
Ксения <ksenia@MacBook-Air.Dlink>
2022-07-07 11:38:27 +0300

    almost done x2

--- src/find_player.c
+++ src/find_player.c
@@ -4,21 +4,21 @@
 
 #include "../includes/cub3d.h"
 
-static void	write_player_data(t_all *s_all, int x, int y, char dir)
-{
-	s_all->plr.x = (double)(x + 0.5);
-	s_all->plr.y = (double)(y + 0.5);
-	if (dir == 'N')
-		s_all->plr.dir = 3 * M_PI_2;
-	else if (dir == 'E')
-		s_all->plr.dir = 2 * M_PI;
-	else if (dir == 'S')
-		s_all->plr.dir = M_PI_2;
-	else if (dir == 'W')
-		s_all->plr.dir = M_PI;
-	else
-		s_all->plr.dir = 3 * M_PI_2;
-}
+//static void	write_player_data(t_all *s_all, int x, int y, char dir)
+//{
+//	s_all->plr.x = (double)(x + 0.5);
+//	s_all->plr.y = (double)(y + 0.5);
+//	if (dir == 'N')
+//		s_all->plr.dir = 3 * M_PI_2;
+//	else if (dir == 'E')
+//		s_all->plr.dir = 2 * M_PI;
+//	else if (dir == 'S')
+//		s_all->plr.dir = M_PI_2;
+//	else if (dir == 'W')
+//		s_all->plr.dir = M_PI;
+//	else
+//		s_all->plr.dir = 3 * M_PI_2;
+//}
 
 void	find_player(t_all *s_all)
 {
@@ -37,6 +37,8 @@ void	find_player(t_all *s_all)
 					|| s_all->map[i][j] == 'E' || s_all->map[i][j] == 'W')
 				{
 					count++;
+					printf("%d", i);
+					printf("%d", j);
 					write_player_data(s_all, j, i, s_all->map[i][j]);
 					s_all->map[i][j] = '0';
 				}
